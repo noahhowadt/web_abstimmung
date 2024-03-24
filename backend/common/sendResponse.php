5ccc9e2f12b37ae46da6ed8affa56ebc103e7df9
Noah Howadt <noah.howadt@gmail.com>
2024-03-24 16:27:13 +0100

    backend create and get appointments

--- backend/common/sendResponse.php
+++ backend/common/sendResponse.php
@@ -1,7 +1,8 @@
 <?php
-
-function sendResponse($status = 200, $data)
+function sendResponse($status, $data = null)
 {
   http_response_code($status);
+  if ($data == null) exit();
+
   echo json_encode($data);
 }
--- backend/create-appointment.php
+++ backend/create-appointment.php
@@ -1,3 +1,30 @@
 <?php
+include "common/sendResponse.php";
+include "common/body.php";
+include "common/db.php";
+include "common/allowedMethods.php";
 
-sendResponse(200, "Hello World!");
+// check if request method is POST
+allowedMethods(["POST"]);
+
+// check if required fields are set
+if (!isset($body["title"]) || !isset($body["location"]) || !isset($body["expires_at"]) || !isset($body["options"]) || !is_array($body["options"]) || count($body["options"]) < 2) {
+  sendResponse(400, ["error" => "Bad Request"]);
+}
+
+// create appointment
+$stmt = $db->prepare("INSERT INTO appointments (title, location, expires_at) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $body["title"], $body["location"], $body["expires_at"]);
+$stmt->execute();
+$stmt->close();
+
+// add voting options
+$appointmentId = $db->insert_id;
+foreach ($body["options"] as $option) {
+  $stmt = $db->prepare("INSERT INTO voting_options (appointment_id, date) VALUES (?, ?)");
+  $stmt->bind_param("is", $appointmentId, $option);
+  $stmt->execute();
+  $stmt->close();
+}
+
+sendResponse(201);
